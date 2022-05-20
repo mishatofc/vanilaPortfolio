20b1f60df950e12bcc061721f72e22b1568a06ee
Mazidul Islam Mishat <officialmishatgaming@gmail.com>
2022-05-20 17:08:47 +0600

    updated a lot of things

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ document.onkeydown = function (e) {
   if (event.keyCode == 123) {
       return false;
   }
-  // disable ctrl+shift+I
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-      return false;
-  }
+ 
   // disable ctrl+shift+C
   if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
       return false;
